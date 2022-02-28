f375e5eb9829ef2fcce30d056c1d8f74c2d23fa1
ColeyG <cjgeerts@gmail.com>
2022-02-27 22:56:17 -0500

    Fetch approach

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
-import puppeteer from "puppeteer";
+// const consoles = ["GB", "GBC", "GBA", "DS", "PSP", "NES", "Genesis", "SNES", "Saturn", "PS1", "N64", "Dreamcast", "PS2", "Xbox", "GameCube", "PS3", "Wii", "WiiWare"];
+// const categories = ["number", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
-console.log(-1);
-const browser = await puppeteer.launch();
+// Testing Data...
+const consoles = ["GB", "GBC", "GBA"];
+const categories = ["number", "A", "B", "C"];
 
-console.log(0);
-const page = await browser.newPage();
 
-console.log(1);
-await page.goto('https://vimm.net/vault/?p=list&system=GB&section=number');
+const scrapePage = async (gameConsole, category) => {
+  const res = await fetch(`https://vimm.net/vault/?p=list&system=${gameConsole}&section=${category}`);
+  const html = await res.text();
 
-console.log(2);
-await page.screenshot({ path: 'example.png' });
+  console.log(html);
+};
 
-console.log(3);
-await browser.close();
+console.log("Starting Vimm's Scraper...\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+
+// Ensuring a synchronous approach
+
+for (const gameConsole of consoles) {
+  console.log(`\nStarting ${gameConsole} Downloads...\n========================================`);
+  for (const category of categories) {
+    console.log(`\nStarting ${gameConsole} Category ${category} Downloads...\n========================================`);
+    await scrapePage(gameConsole, category);
+  }
+}
+
+console.log("\nFinished Scraping!");
